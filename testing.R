23b34f83c220b7456e17392260291cca07b1d315
Alessandro Gasparini <alessandro.gasparini@reddooranalytics.se>
2022-11-11 12:18:33 +0100

    Added file for testing

---
+++ testing.R
@@ -0,0 +1,19 @@
+# File for testing
+library(msm)
+
+# Builds upon the example in ?msm
+twoway4.q <- rbind(
+  c(-0.5, 0.25, 0, 0.25), c(0.166, -0.498, 0.166, 0.166),
+  c(0, 0.25, -0.5, 0.25), c(0, 0, 0, 0)
+)
+cav.msm <- msm(
+  formula = state ~ years,
+  subject = PTNUM,
+  data = cav,
+  qmatrix = twoway4.q,
+  deathexact = 4
+)
+
+# Create a dataset with the predictions
+df <- stacked.data(model = cav.msm, tstart = 0, tforward = 1, tseqn = 5)
+df

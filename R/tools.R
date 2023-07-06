b9c0799a45552384dea8ba103436cf7d7280ac35
Alessandro Gasparini <alessandro.gasparini@reddooranalytics.se>
2023-07-06 14:16:13 +0200

    Rewriting internals for licensing purposes

--- R/tools.R
+++ R/tools.R
@@ -13,17 +13,23 @@
 
 #' @keywords internal
 .resample_models <- function(x, B) {
-  # Code based on msm:::normboot.msm()
+  # See msm:::normboot.msm() for a more extensive implementation
+  #
+  # Check that x$paramdata$fixedpars, x$paramdata$hmmpars have length = 0
+  # This way we don't have to support models with fixed parameters or hidden markov models
+  if (length(x$paramdata$fixedpars) > 0) stop("Models with constraints (argument `fixedpars` of msm()) are not supported by this functionality.", call. = FALSE)
+  if (length(x$paramdata$hmmpars) > 0) stop("Hidded Markov models are not supported by this functionality.", call. = FALSE)
+  # Resample
   sim <- mvtnorm::rmvnorm(B, x$opt$par, x$covmat[x$paramdata$optpars, x$paramdata$optpars])
   params <- matrix(nrow = B, ncol = x$paramdata$npars)
   params[, x$paramdata$optpars] <- sim
-  params[, x$paramdata$fixedpars] <- rep(x$paramdata$params[x$paramdata$fixedpars], each = B)
-  params[, x$paramdata$hmmpars] <- rep(msm:::msm.mninvlogit.transform(x$paramdata$params[x$paramdata$hmmpars], x$hmodel), each = B)
-  params <- params[, !duplicated(abs(x$paramdata$constr)), drop = FALSE][, abs(x$paramdata$constr), drop = FALSE] * rep(sign(x$paramdata$constr), each = B)
-  sim.stat <- vector(B, mode = "list")
-  for (i in 1:B) {
-    x.rep <- msm::updatepars.msm(x, params[i, ])
-    sim.stat[[i]] <- stat(x.rep)
-  }
-  return(sim.stat)
+  # Create a list
+  out <- lapply(
+    X = seq(B),
+    FUN = function(i) {
+      this <- msm::updatepars.msm(x, params[i, ])
+      stat(this)
+    }
+  )
+  return(out)
 }
